14afe5ce2f586ac4c38183b1149265a531c7244e
Verushan Naidoo <verushannaidoo@gmail.com>
2022-03-30 21:59:43 +0200

    Add name Verushan

--- Program.java
+++ Program.java
@@ -3,5 +3,6 @@ class Program
     public static void main(String[] args)
     {
         System.out.println("Hello World"); // Show hello world to the user
+        System.out.println("This is Verushan");
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a test project to make proper use of Git and Github
+
+Go ahead and add a print statement saying "This is {}" where your name is in the place of the curly braces.

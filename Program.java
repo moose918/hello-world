69854f0e86127ef8a714a9277f6598a7bdebecde
Verushan Naidoo <verushannaidoo@gmail.com>
2022-03-30 19:11:24 +0200

    Add readme

--- Program.java
+++ Program.java
@@ -2,6 +2,6 @@ class Program
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); // Show hello world to the user
     }
 }
